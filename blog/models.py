54c957c11edfd04a910fefdf44def8388e102ada
omg666 <utrique@gmail.com>
2017-09-10 09:34:22 +0200

    Added forms for editing and adding new posts.

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
 		default=timezone.now)
 	published_date=models.DateTimeField(
 		blank=True, null=True)
-	category=models.TextField(100)
+	category=models.CharField(max_length=100)
 
 	def publish(self):
 		self.published_date=timezone.now()
